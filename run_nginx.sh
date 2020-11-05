136063bdc10a7bd2f810cc2dfddfb731ff0de76c
ripka <ripka@google.com>
2020-11-05 23:06:52 +0000

    add some anthos specific code

--- run_nginx.sh
+++ run_nginx.sh
@@ -5,6 +5,12 @@ else
 	export NGINX_PORT=$PORT
 fi
 #envsubst < /etc/nginx/sites-enabled/default > /etc/nginx/sites-enabled/default
+#this step creates nginx required logging directory for Anthos Cloud Run 
+if [ -d "/var/log/nginx" ]; then
+    echo "directory exists"
+else
+    mkdir /var/log/nginx
+fi
 echo "pre sed"
 sed -i 's/\$NGINX_PORT/'$PORT'/g' /etc/nginx/sites-enabled/default
 echo "post sed"
